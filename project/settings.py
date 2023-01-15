e1c809dfc054eb59a7fa558c9530de62f0509e52
Kousik <kousiknandu16@gmail.com>
2023-01-15 19:46:40 +0530

    commit

--- project/settings.py
+++ project/settings.py
@@ -113,7 +113,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'project/static/'
 
 
 #STATICFILES_DIRS  =  [
@@ -132,5 +132,5 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 import os
-STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
+STATICFILES_DIRS = os.path.join(BASE_DIR,'project/static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
